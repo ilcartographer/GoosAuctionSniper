30ff4cfee8a9d0976da3824e8aab1c4881bd16c3
Marshall <Marshall.D.Miller@disney.com>
2021-09-02 10:58:55 -0400

    Add initial end-to-end test
    Ch 10: The Walking Skeleton
    This is the initial commit for the project, including project setup and the initial failing end-to-end test (p.85).

---
+++ src/test/java/AuctionSniperEndToEndTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+public class AuctionSniperEndToEndTest {
+    private final FakeAuctionServer auction = new FakeAuctionServer("item-54321");
+    private final ApplicationRunner application = new ApplicationRunner();
+
+    @Test
+    public void sniperJoinsAuctionUntilAuctionClose() throws Exception {
+        auction.startSellingItem();
+        application.startBiddingIn(auction);
+        auction.hasReceivedJoinRequestFromSniper();
+        auction.announceClosed();
+        application.showsSniperHasLostAuction();
+    }
+
+    @AfterEach
+    public void stopAuction() {
+        auction.stop();
+    }
+
+    @AfterEach
+    public void stopApplication() {
+        application.stop();
+    }
+}
